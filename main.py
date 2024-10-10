837f7702020074dbbae5d09999b0a0879d291f74
issamoxix <issamhaidaoui@gmail.com>
2024-10-11 01:25:05 +0200

    final touch

--- main.py
+++ main.py
@@ -49,6 +49,15 @@ def main():
     ]
 
     df = pd.concat([eventbrite_df, eventim_df])
+
+    eventim_df = eventim_df[(eventim_df["lat"] < 52.5) & (eventim_df["lon"] < 13.5)]
+    df = df[(df["lon"] < 13.5) & (df["lat"] < 52.5)]
+    # df[
+    #     (df["lat"] >= lat_min)
+    #     & (df["lat"] <= lat_max)
+    #     & (df["lon"] >= lon_min)
+    #     & (df["lon"] <= lon_max)
+    # ]
     df["unified_tags"] = df.apply(
         lambda row: unify_tags(row, "categories1name", "tags1display_name", mapping),
         axis=1,
@@ -74,12 +83,9 @@ def main():
 
     roi_df = dataframes[mapstyle]
     if tag != "All":
+        roi_df = dataframes["All"]
         roi_df = roi_df[roi_df["unified_tags"] == tag]
-    # if mapstyle == "EventBrite":
-        # roi_df = roi_df[roi_df["ticket_availability.maximum_ticket_price.currency"] == "EUR"]
-        # st.map(roi_df, color="color", size="average_ticket_price", hex=)
-    # else:
-    st.map(roi_df, color="color", size=20)
+    st.map(roi_df, color="color", size=20, zoom=10)
 
     frequent_source = {
         "EventBrite": "tags1display_name",
@@ -112,59 +118,80 @@ def main():
     plt.ylabel("Categories")
     st.pyplot(fig)
 
-    tag_duration = df.groupby("tags1display_name")["event_duration"].mean()
-    duration_fig = plt.figure(figsize=(10, 6))
-    tag_duration.plot(kind="bar", color="lightgreen")
-
-    plt.title("Average Event Duration by Tags")
-    plt.xlabel("Tags")
-    plt.ylabel("Average Duration (Days)")
-    plt.xticks(rotation=45, ha="right")
-
-    plt.tight_layout()
-    st.subheader("Duration Frequency")
+    # average event duration by tags
+    st.subheader("Duration Frequency by Tags")
     st.markdown(
         """
         This chart shows the average duration of events based on the selected source.
         """
     )
+
+    tag_duration = df.groupby("tags1display_name")["event_duration"].mean()
+    duration_fig = plt.figure(figsize=(10, 6))
+    tag_duration.plot(kind="bar", color="#cf663f")
+
+    plt.title("EventBrite Average Event Duration by Tags")
+    plt.xlabel("Tags")
+    plt.ylabel("Average Duration (Days)")
+    plt.xticks(rotation=45, ha="right")
+
+    plt.tight_layout()
     st.pyplot(duration_fig)
+
+    _eventim_tag_duration = df.groupby("categories1name")["event_duration"].mean()
+
+    _eventim_tag_duration_fig = plt.figure(figsize=(10, 6))
+    _eventim_tag_duration.plot(kind="bar", color="#232864")
+
+    plt.title("Eventim Average Event Duration by Tags")
+    plt.xlabel("Tags")
+    plt.ylabel("Average Duration (Days)")
+    plt.xticks(rotation=45, ha="right")
+
+    plt.tight_layout()
+    st.pyplot(_eventim_tag_duration_fig)
 
     is_sold_out = {
         "Platform": ["EventBrite", "Eventtim"],
         "Average Sold Out (%)": [5.506607929515418, 0.9],
-        'Online Events (%)': [24, None]
+        "Online Events (%)": [24, None],
     }
     sold_out_df = pd.DataFrame(is_sold_out)
-    st.subheader("Average Sold Out (%)")
+    st.subheader("Average Sold Out (%) and Online Events (%)")
     st.table(sold_out_df)
 
     eventbrite_df.groupby("primary_venue.name").count()
 
     st.subheader("Top 10 Venues with Most Events")
+    st.markdown(
+        """
+        This dashboard provides insights into the most popular event venues, helping organizers identify key locations for hosting events. The displayed table showcases the top 10 venues based on event counts, excluding the most prominent venue. This information aids in understanding venue preferences and trends, enabling data-driven decisions for future event planning.
+"""
+    )
     cols = st.columns(2)
-    
+
     eventbrite_df["Venue"] = eventbrite_df["primary_venue.name"]
-    eventim_df["Venue"] = eventim_df["products0typeAttributes0liveEntertainment0location0name"]
+    eventim_df["Venue"] = eventim_df[
+        "products0typeAttributes0liveEntertainment0location0name"
+    ]
 
     with cols[0]:
-        st.html("<h5>EventBrite</h5>")
-        venue_counts= eventbrite_df.groupby("Venue").size()
+        st.html("<h5 style='color:#cf663f;'>EventBrite</h5>")
+        venue_counts = eventbrite_df.groupby("Venue").size()
         sorted_vanues = venue_counts.sort_values(ascending=False)
         top_10 = sorted_vanues.iloc[1:11]
-        top_10_df = top_10.reset_index(name='Count')
+        top_10_df = top_10.reset_index(name="Count")
         top_10_df.index += 1
         st.table(top_10_df)
-    
+
     with cols[1]:
-        st.html("<h5>Eventim</h5>")
-        _venue_counts= eventim_df.groupby("Venue").size()
+        st.html("<h5 style='color:#232864;'>Eventim</h5>")
+        _venue_counts = eventim_df.groupby("Venue").size()
         _sorted_vanues = _venue_counts.sort_values(ascending=False)
         _top_10 = _sorted_vanues.iloc[1:11]
-        _top_10_df = _top_10.reset_index(name='Count')
+        _top_10_df = _top_10.reset_index(name="Count")
         _top_10_df.index += 1
         st.table(_top_10_df)
-
 
 
 if __name__ == "__main__":
